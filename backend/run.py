d507fd4996d2cd4b536f836de5391493eaf944b0
Tiew <tnttiew@gmail.com>
2025-07-10 11:03:47 +0700

    fix: Update run.py to use environment variable for port configuration

--- backend/run.py
+++ backend/run.py
@@ -2,4 +2,4 @@ import subprocess
 from pathlib import Path
 import os
 
-subprocess.run(["python", "-m", "uvicorn", "app:app", "--host", "0.0.0.0", "--port", "10000"], cwd=str(Path(os.getcwd()) / "backend" / "app"))
+subprocess.run(["python", "-m", "uvicorn", "app:app", "--host", "0.0.0.0", "--port", str(os.getenv("PORT", 10000))], cwd=str(Path(os.getcwd()) / "backend" / "app"))

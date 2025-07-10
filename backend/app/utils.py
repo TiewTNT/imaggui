81aa82ec40e8a2c0f7354a5234c539bd4ba06e5b
Tiew <tnttiew@gmail.com>
2025-07-10 22:43:30 +0700

    fix: Suffix fallback in get_image_format

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -10,6 +10,9 @@ def get_image_format(path: Path) -> str:
     )
     print('[INFO] Attempting', str(fr'magick "{str(path)}" -format "%m\n" info:'))
     detected = result.stdout.strip().lower()
+    if not detected:
+        print('[INFO] Using suffix fallback')
+        detected = path.suffix[1:]
     print('[INFO] The format of', str(path), 'is', detected)
     print('[INFO]', path, 'exists =', path.exists())
     return detected

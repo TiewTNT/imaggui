9f2d318b8152b97c6a96f31452c630bced02ca70
Tiew <tnttiew@gmail.com>
2025-07-15 17:36:00 +0700

    Added better license formatting!

--- frontend/scour.py
+++ frontend/scour.py
@@ -1,5 +1,7 @@
+import markdown
 from pathlib import Path
 node_modules = Path('./node_modules')
+
 
 def safe_read(file: Path) -> str:
     encodings_to_try = ['utf-8', 'utf-16', 'utf-32', 'latin-1']
@@ -10,6 +12,7 @@ def safe_read(file: Path) -> str:
             continue
     # Final fallback with errors replaced
     return file.read_text(encoding='utf-8', errors='replace')
+
 
 with open('+page.svelte', 'a', encoding='utf-8') as licenses:
     licenses.write("""<script>
@@ -22,24 +25,27 @@ with open('+page.svelte', 'a', encoding='utf-8') as licenses:
     <section
         class="min-h-screen bg-surface-900 text-surface-100 px-6 py-16 flex justify-center"
     >
-                    <div class="max-w-3xl w-full space-y-10">
+                    <div class="w-full max-w-3xl px-4 sm:px-6 lg:px-8 space-y-8">
                    <h1 class="text-4xl font-extrabold mb-8 text-center text-primary-400">
                            Third Party Licenses
                            </h1> """)
     for file in node_modules.rglob('*'):
-        if file.stem.lower().strip() == 'license':
+        if file.stem.lower().strip() == 'license' and file.suffix not in ['js', 'ts', 'css', 'ico', 'svg', 'png', 'jpg', 'webp', 'tiff']:
             try:
                 package = file.parts[file.parts.index('node_modules') + 1]
             except ValueError:
                 package = "UNKNOWN"
             text = safe_read(file)
-            text = '<p class="text-lg leading-relaxed">'+text.replace('>', '&gt;').replace('<', '&lt;').replace('{', '&lbrace;').replace('}', '&rbrace;').replace('\n', '<br/>')+'</p>\n'
-        
+            text = markdown.markdown(text.replace('>', '&gt;').replace('<', '&lt;').replace(
+                '{', '&lbrace;').replace('}', '&rbrace;').replace('\n', '<br/>'))
+            text = text.replace('<p>', '<p class="text-base sm:text-lg leading-relaxed break-words">').replace(
+                '<a ', '<a class="text-primary-400 italic" ')+'\n'
+
             licenses.write(f'''<h2 class="text-3xl font-extrabold mb-8 text-center text-primary-400">
                            License from: {package}
                            </h2> 
                            {text}
-                           <div class=\"flex justify-center\"
-                           ><hr class=\"w-[30%]\" />
-                           </div>''')
+                           <div class="my-8 flex justify-center">
+  <hr class="w-1/2 sm:w-1/3" />
+</div>''')
     licenses.write("</div></section></section>")
